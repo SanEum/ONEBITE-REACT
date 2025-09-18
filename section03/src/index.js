2a35b4042fba040f64cbaf4579d54c83019fd137
Saneum <code4696@naver.com>
2025-09-18 19:12:04 +0900

    3.chapter05 라이브러리

--- section03/src/index.js
+++ section03/src/index.js
@@ -1,5 +1,10 @@
 // const { add, sub } = require("./math"); // commonJS module
 import mul, { add, sub } from "./math.js";
-console.log(add(1, 2));
-console.log(sub(1, 2));
-console.log(mul(2, 3));
+// console.log(add(1, 2));
+// console.log(sub(1, 2));
+// console.log(mul(2, 3));
+
+import randomColor from "randomcolor";
+
+const color = randomColor();
+console.log(color);
