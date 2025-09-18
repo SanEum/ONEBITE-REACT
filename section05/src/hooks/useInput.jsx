e926c5aec33c2d4547f23d79a32d03575921e0e8
Saneum <code4696@naver.com>
2025-09-19 03:23:04 +0900

    5.chapter11 hook
     - 커스텀 훅 만들 수 있다(use)
     - 훅은 커스텀 훅 or 함수 컴포넌트 내에서 가능
     - 조건부 호출 불가

---
+++ section05/src/hooks/useInput.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+function useInput() {
+    const [input, setInput] = useState();
+
+    function onChange(e) {
+        setInput(e.target.value);
+    }
+    return [input, onChange];
+}
+
+export default useInput;
