19deee45939a4ab4e2218a3c01fdfddcd23fe942
Saneum <code4696@naver.com>
2025-09-18 18:56:54 +0900

    3.chapter04 모듈 시스템
     - commonJS: module.exports, require
     - ES: export, import

---
+++ section03/src/math.js
@@ -0,0 +1,20 @@
+// math 모듈
+
+export function add(a, b) {
+    return a + b;
+}
+
+export function sub(a, b) {
+    return a - b;
+}
+
+// module.exports = { // commonJS module
+//     add,
+//     sub,
+// };
+
+// export { add, sub };
+
+export default function multyply(a, b) {
+    return a * b;
+}
